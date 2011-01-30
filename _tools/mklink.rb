a477ecfd5f032fd5bc9481c71dd9f330459209da
troter <trot.thunder@gmail.com>
2011-01-30 20:08:49 +0900

    add mklink.rb and sample data

---
+++ _tools/mklink.rb
@@ -0,0 +1,40 @@
+# -*- coding:utf-8 -*-
+
+require 'yaml'
+$KCODE = "U"
+
+module LinkMager
+
+  class RedirectMaker
+    def initialize(config)
+      @dirname = File.dirname(config)
+      @config = YAML.load_file(config)
+    end
+
+    def run
+      @config.each do |data|
+        make_link(data)
+      end
+    end
+
+    def make_link(data)
+      name = data['name']
+      html = <<EOS
+<html>
+<meta http-equiv="REFRESH" content="0;URL=#{data['url']}">
+</html>
+EOS
+      File.open("#{@dirname}/#{name}.html", "w") do |f|
+        f << html
+      end
+    end
+  end
+
+  def self.run
+    Dir["../**/link.yml"].each do |config|
+      LinkMager::RedirectMaker.new(config).run
+    end
+  end
+end
+
+LinkMager.run
